6b79b19a8de3fad359d2cafb38687e5aa4ddb126
Kshitij Goel <kshitijg9899@gmail.com>
2018-07-08 17:26:28 +0530

    final

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require("express");
 var todoController = require("./controllers/todoController");
+var PORT = process.env.PORT || 3000;
 var app = express();
 app.set("view engine", "ejs");
 app.use(express.static("./public"));
 todoController(app);
-app.listen(3000);
+app.listen(PORT);
 console.log("You are listening to port 3000");
+////
